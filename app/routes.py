a4d8bcf545cdda1772a6a4ce434e85328c9c4273
Jody Sheffield <jodycontigo@gmail.com>
2022-08-03 08:29:04 -0500

    Second Commit 8-3-22

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, request
 import requests
 from app import app
+from app.forms import FindPokemon
 
 @app.route('/', methods=['GET'])
 def index():
@@ -8,18 +9,25 @@ def index():
 
 @app.route('/pokemon', methods=['GET', 'POST'])
 def pokemon():
-    if request.method == 'POST':
-        name = request.form.get('name')
-
-        url = f"https://pokeapi.co/api/v2/pokemon/{name}"
+    form = FindPokemon()
+    if request.method =='POST':
+        pokemon_name = form.pokemon_name.data.lower()
+        url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}/'
         response = requests.get(url)
-        
+        if not response.ok:
+            error_string = "No Pokemon Found, Try Again..."
+            return render_template('pokemon.html.j2', error=error_string,form=form)
         data = response.json()
-
-        poke_dict={
-            "name": data.json()['name'].title(),
-        }
-        return render_template('index.html.j2', pokes=poke_dict)
-    else:
-        error = 'error'
-        return render_template('index.html.j2', poke=error)
+        for pokemon in data:
+            poke_dict={}
+            poke_dict={
+                "name": data['name'].title(),
+                "ability":data['abilities'][0]["ability"]["name"],
+                "base experiance":data['base_experience'],
+                "photo":data['sprites']['other']['home']["front_default"],
+                "attack base stat": data['stats'][1]['base_stat'],
+                "hp base stat":data['stats'][0]['base_stat'],
+                "defense stat":data['stats'][2]["base_stat"]
+            }
+        return render_template('pokemon.html.j2', pokemon=poke_dict, form=form) 
+    return render_template('pokemon.html.j2', form=form)
--- config.py
+++ config.py
@@ -2,4 +2,3 @@ import os, json
 # Config
 class Config():
     SECRET_KEY=os.environ.get('SECRET_KEY')
-    REGISTERED_USERS=json.loads(os.environ.get('REGISTERED_USERS'))
